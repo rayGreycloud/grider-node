7a9a552e6f0f83de59962ef13170c7af8f04457b
rayGreycloud <rayGreycloud@gmail.com>
2018-03-24 10:28:41 -0700

    Add node cluster and clustering example

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+const cluster = require('cluster');
 const express = require('express');
 
-const app = express();
-const port = 3000;
+// Master mode? 
+if (cluster.isMaster) {
+  // Create child mode instance
+  cluster.fork();
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  
+} else {
+  // If child, act like server
+  const app = express();
+  const port = 3000;
+  
+  // Pseudo work load 
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+      // Run whle loop until duration passes
+    }
+  }
 
-app.get('/', (req, res) => {
-  res.send('Ding.');  
-});
+  app.get('/', (req, res) => {
+    doWork(4000);
+    res.send('Ding!');  
+  });
+  
+  app.get('/fast', (req, res) => {
+    res.send(`Ding! I'm fast!`);  
+  });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });  
+}
