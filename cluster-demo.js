acaa254f9d4262f2807ffc9bf3ea2d37ed475d66
rayGreycloud <rayGreycloud@gmail.com>
2018-03-29 09:28:28 -0700

    Rename cluster-demo

--- cluster-demo.js
+++ cluster-demo.js
@@ -6,24 +6,13 @@ if (cluster.isMaster) {
   // Create child mode instance
   cluster.fork();
   cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
   
 } else {
   // If child, act like server
   const app = express();
   const port = 3000;
   
-  // Pseudo work load 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {
-      // Run whle loop until duration passes
-    }
-  }
-
   app.get('/', (req, res) => {
-    doWork(4000);
     res.send('Ding!');  
   });
   
